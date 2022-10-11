9c8a6c5c2f3262b4e39e05c568018ebac669542f
EiraToral <eiratoral.buap@gmail.com>
2022-10-10 22:13:06 -0500

    Se modificó los elementos de entrada de la función SGD, se realizaron diversas pruebas cambiando los valores de cada variable, al final obteniendo estos valores mas efiicientes para epoch 20, minibatch 10 y tamaño de paso 7.0

--- Pruebas/Main NN.py
+++ Pruebas/Main NN.py
@@ -5,7 +5,9 @@ training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
 net=network.Network([784,30,10])
-net.SGD( training_data, 30, 10, 3.0, test_data=test_data)
+#Variación de los elementos de entrada para prueba.
+# Se encontró que una buena combinación de los parámetros obtieniendo porcentajes de acierto de hasta 95.06
+net.SGD( training_data, 20, 10, 7.0, test_data=test_data)
 archivo = open("red_prueba1.pkl",'wb')
 pickle.dump(net,archivo)
 archivo.close()
